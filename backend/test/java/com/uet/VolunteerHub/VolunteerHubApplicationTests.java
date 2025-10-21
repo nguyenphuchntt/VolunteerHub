b2f6165447e54b36c2bfd88b193827e06a2fcb92
snguyenanh1 <snguyenanh1@gmail.com>
2025-10-21 22:59:08 +0700

    First commit

---
+++ backend/test/java/com/uet/VolunteerHub/VolunteerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uet.VolunteerHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
