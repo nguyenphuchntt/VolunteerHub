66086d74b338f4c82cee25dd33bb5ad5f5173755
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2025-10-29 23:37:41 +0700

    Update: Notification types

--- backend/src/main/java/com/uet/VolunteerHub/enums/NotificationType.java
+++ backend/src/main/java/com/uet/VolunteerHub/enums/NotificationType.java
@@ -1,7 +1,8 @@
 package com.uet.VolunteerHub.enums;
 
 public enum NotificationType {
-    FROM_USER,
-    FROM_ADMIN,
-    FROM_MANAGER
+    URGENT,
+    ANNOUNCEMENT,
+    REMINDER,
+    NORMAL
 }
