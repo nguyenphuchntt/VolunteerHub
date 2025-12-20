4bbcc456eb1d8c6af02e96fe0e284e2f08b5b6aa
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2025-12-20 20:48:32 +0700

    add: notification DTOs

--- backend/src/main/java/com/uet/VolunteerHub/dto/NotificationReadDTO.java
+++ backend/src/main/java/com/uet/VolunteerHub/dto/NotificationReadDTO.java
@@ -1,20 +1,29 @@
 package com.uet.VolunteerHub.dto;
 
+import com.uet.VolunteerHub.enums.DestinationType;
 import com.uet.VolunteerHub.enums.NotificationType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class NotificationReadDTO {
     private Long notificationId;
     private UUID senderAccountId;
+    private String senderUsername;
     private UUID receiverAccountId;
     private String content;
     private Boolean isRead;
     private NotificationType type;
+    private DestinationType destinationType;
+    private String destinationId;
+    private OffsetDateTime createdAt;
 }
+
