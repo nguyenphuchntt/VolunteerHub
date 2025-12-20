f8c2a8e715c13a68a9e9837f29d0a26d7777b1bd
Anh-Son Nguyen <138212930+snguyenanh1@users.noreply.github.com>
2025-12-20 11:49:09 +0700

    Web push api (#46)
    * fix bug
    
    * fix bug
    
    * fix follow bug
    
    * push class
    
    * Add web push logic
    
    * fix bug

---
+++ backend/src/main/java/com/uet/VolunteerHub/configuration/FirebaseConfiguration.java
@@ -0,0 +1,42 @@
+package com.uet.VolunteerHub.configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+@EnableConfigurationProperties(FirebaseProperties.class)
+public class FirebaseConfiguration {
+    @Bean
+    FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+    @Bean
+    FirebaseApp firebaseApp(GoogleCredentials credentials) {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    GoogleCredentials googleCredentials(FirebaseProperties firebaseProperties) throws IOException {
+        if (firebaseProperties.getServiceAccount() != null) {
+            try (InputStream is = firebaseProperties.getServiceAccount().getInputStream()) {
+                return GoogleCredentials.fromStream(is);
+            }
+        } else {
+            return GoogleCredentials.getApplicationDefault();
+        }
+    }
+
+}
