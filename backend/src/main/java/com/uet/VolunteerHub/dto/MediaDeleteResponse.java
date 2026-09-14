4c303ebca43798cf27946c9375930e5c8d94f203
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2026-09-14 18:48:55 +0700

    update: update entity

--- backend/src/main/java/com/uet/VolunteerHub/dto/MediaDeleteResponse.java
+++
@@ -1,8 +0,0 @@
-package com.uet.VolunteerHub.dto;
-
-import java.util.UUID;
-
-public record MediaDeleteResponse(
-        String message,
-        UUID id) {
-}
--- backend/src/main/java/com/uet/VolunteerHub/repository/PostRepository.java
+++ backend/src/main/java/com/uet/VolunteerHub/repository/PostRepository.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
-    //    Page<Post> findByTitleContainingIgnoreCase(String title, Pageable pageable);
     Optional<Post> findByPostId(Long postId);
     Page<Post> findByEvent_EventId(Long eventId, Pageable pageable);
 }
